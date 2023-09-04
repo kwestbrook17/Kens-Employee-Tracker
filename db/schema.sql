5140d4744f43161895338e4e1d15042dcd7438d8
Kenneth Westbrook <kennethwestbrook17@gmail.com>
2023-09-03 21:01:20 -0700

    schema tables

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+USE company_db;
+
+-- Department Table
+CREATE TABLE department (
+  id INT PRIMARY KEY,
+  name VARCHAR(30)
+);
+
+-- Role Table
+CREATE TABLE role (
+  id INT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+-- Employee Table
+CREATE TABLE employee (
+  id INT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
